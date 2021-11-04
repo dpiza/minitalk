b02ad1dfd28c9d1ff4474dc9c35e8cc8043a546a
Denny Piza <dpiza@student.42sp.org.br>
2021-11-04 21:31:56 +0100

    removes signal func from send_signal; relocates g_ack 0 set

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 18:27:58 by dpiza             #+#    #+#             */
-/*   Updated: 2021/11/04 03:31:07 by dpiza            ###   ########.fr       */
+/*   Updated: 2021/11/04 21:24:06 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,13 @@ void	send_signal(int pid, char *str)
 		i = 8;
 		while (i-- > 0)
 		{
-			g_ack = 0;
 			if (*str & 1 << i)
 				kill (pid, SIGUSR1);
 			else
 				kill (pid, SIGUSR2);
-			signal(SIGUSR1, acknowledge);
 			while (!g_ack)
 				pause();
-			usleep(100);
+			g_ack = 0;
 		}
 		str++;
 	}
@@ -53,6 +51,7 @@ int	main(int argc, char **argv)
 		ft_printf("Usage: ./client [SRV_PID] [STRING]\n");
 		return (EXIT_FAILURE);
 	}
+	signal(SIGUSR1, acknowledge);
 	send_signal(ft_atoi(argv[1]), argv[2]);
 	return (0);
 }
